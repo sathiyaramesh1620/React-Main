33f65470fe367b8c28dd44ceca6e50183c00ff26
sathiyaramesh <sathiyaramesh1620@gmail.com>
2022-10-28 22:04:01 +0530

    done

--- react-11/src/App.js
+++ react-11/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import Registration from "./Form Handlings/Registration";
-class App extends React.Component{
-    render(){
-        return<div>
-            <Registration/>
+import React from 'react'
+import Navbar from './navbars/navbar'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+const App = () => {
+    return (
+        <div>
 
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </Router>
         </div>
-    }
+    )
 }
 export default App
